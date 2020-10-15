dc922b7bfc4316773fd5f2d65803cc0af12a8bef
ValentinoUberti <vuberti@redhat.com>
2020-10-15 19:30:49 +0200

    Memory consumption simulator

--- demo-docker/src/main/java/com/example/demodocker/DemoController.java
+++ demo-docker/src/main/java/com/example/demodocker/DemoController.java
@@ -1,5 +1,5 @@
 package com.example.demodocker;
-
+import java.util.Vector;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +13,20 @@ public class DemoController
     public List<Customer> findAll()
     {
         List<Customer> customerList = new ArrayList<Customer>();
-        customerList.add(new Customer(1, "frank"));
-        customerList.add(new Customer(2, "john"));
+        customerList.add(new Customer(1, "Demo"));
+        customerList.add(new Customer(2, "Test"));
+
+
+        // Simulating a 10Mib memory consumption and a cpu workload
+        Vector v = new Vector();
+        
+        for (int i=0; i <= 1024*1024; i++ )
+          {
+              byte b[]= new byte[1];
+              v.add(b);
+          }
+
+
         return customerList;
     }
 }
